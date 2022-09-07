3f62a8435216c904d1da48f13dd90c173b51e713
Joaco96 <joacocortes96@gmail.com>
2022-09-07 18:50:49 -0300

    clase05 completada2

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-import checkComplete from "./components/checkComplete.js";
-import deleteIcon from "./components/deleteIcon.js";
+import {addTask} from "./components/addTask.js"
+import {readTasks} from "./components/readTasks.js";
 
 const btn = document.querySelector("[data-form-btn]");
 
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card");
-    const taskContent = document.createElement("div");
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    task.appendChild(taskContent);
-    task.appendChild(deleteIcon());
-    list.appendChild(task);
-    input.value = "";
-};
+btn.addEventListener("click", addTask);
 
-btn.addEventListener("click", createTask);
+readTasks();
